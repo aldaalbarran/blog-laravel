b43a116504ab0d2e639efaa73ef95012fb5ea190
Aldair Camacho Albarran <aldaalbarran.dev@gmail.com>
2023-03-27 00:44:29 -0600

    Factories y Seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Aldair Camacho Albarran',
+            'email' => 'aldaxalbarran@gmail.com',
+            'password' => bcrypt('123456789')
+        ]);
+        User::factory(99)->create();
+    }
+}
