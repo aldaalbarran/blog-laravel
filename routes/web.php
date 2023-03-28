b49387ac2fe62b5d293837de7669abfa487cbdce
Aldair Camacho Albarran <aldaalbarran.dev@gmail.com>
2023-03-27 21:30:32 -0600

    Front-End: posts.show

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', [PostController::class, 'index'])->name('post.index');
 
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
