5deae4b1fdfa583bbe0624605e8e0c260b8011d4
Aldair Camacho Albarran <aldaalbarran.dev@gmail.com>
2023-03-29 15:45:01 -0600

    Back-End: CRUD - etiquetas

---
+++ resources/views/admin/tags/index.blade.php
@@ -0,0 +1,58 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Lista De Etiquetas</h1>
+    <a href="{{ route('admin.tags.create') }}" class="btn btn-secondary">Nueva Etiqueta</a>
+@stop
+
+@section('content')
+
+    @if (session('info'))
+        <div id="session-alert" class="alert alert-success">
+            <strong>{{ session('info') }}</strong>
+        </div>
+    @endif
+
+    <div class="card">
+        <div class="card-body">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th colspan="2"></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach ($tags as $tag)
+                        <tr>
+                            <td>{{ $tag->id }}</td>
+                            <td>{{ $tag->name }}</td>
+                            <td width="10px">
+                                <a href="{{ route('admin.tags.edit', $tag) }}" class="btn btn-primary btn-sm">Editar</a>
+                            </td>
+                            <td width="10px">
+                                <form action="{{ route('admin.tags.destroy', $tag) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@stop
+
+@section('js')
+    <script>
+        setTimeout(function(){
+            $('#session-alert').fadeOut('fast');
+        }, 3000);
+    </script>
+@stop
